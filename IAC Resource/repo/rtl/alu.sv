d43126096d60bbb4337518f304649e698c347ca6
= <zain@ZainLaptop>
2025-11-14 14:15:52 +0000

    alu inputs + outputs

---
+++ IAC Resource/repo/rtl/alu.sv
@@ -0,0 +1,9 @@
+module alu(
+    input logic aluop1 [31:0],
+    input logic aluop2 [31:0],
+    input logic aluctrl [1:0],
+    output logic aluout [31:0],
+    output logic eq 
+);
+    
+endmodule
